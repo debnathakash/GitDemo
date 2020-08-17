62d808d8d04a8dad9f371ba578c750333b25d7bb
Akash Debnath <yoyoakash.cool@hotmail.com>
2020-08-17 12:59:53 +0530

    Commiting agsin

---
+++ src/test/java/qaclickacademy/mavenjava/ProtractorTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class ProtractorTest {
+
+	@Test
+	public void protractor() {
+		System.out.println("Used for Testing Angular");
+		System.out.println("Changes done by Asian architect");
+		System.out.println("Commit");
+	}
+}
