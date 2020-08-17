1874490ef20556f0049350d00c22235f4731858e
Akash Debnath <yoyoakash.cool@hotmail.com>
2020-08-17 12:21:56 +0530

    Changes done by American Architect

--- src/test/java/qaclickacademy/mavenjava/SeleniumTest.java
+++ src/test/java/qaclickacademy/mavenjava/SeleniumTest.java
@@ -7,5 +7,7 @@ public class SeleniumTest {
 	@Test
 	public void SelAuto() {
 		System.out.println("Runs browser automation");
+		System.out.println("Used basically to test UI");
+		System.out.println("Changes done by american architect");
 	}
 }
