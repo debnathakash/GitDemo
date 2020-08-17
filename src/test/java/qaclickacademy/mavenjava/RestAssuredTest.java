cf8dc4118280d2ef0d4582400f4112e64d406eeb
Akash Debnath <yoyoakash.cool@hotmail.com>
2020-08-17 11:14:27 +0530

    Changes done by American Guy

--- src/test/java/qaclickacademy/mavenjava/RestAssuredTest.java
+++ src/test/java/qaclickacademy/mavenjava/RestAssuredTest.java
@@ -8,5 +8,7 @@ public class RestAssuredTest {
 	public void apiTesting() {
 		System.out.println("Used for api testing");
 		System.out.println("Postman is used for api tersting");
+		//Now the American guy is going to change the project
+		System.out.println("Changes done By American guy");
 	}
 }
